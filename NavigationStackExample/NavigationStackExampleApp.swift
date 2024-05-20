d0affc478bc6c51fb21dfea83dd479662c90169f
Safwen <safwen.debbichi-prestataire@bforbank.com>
2024-05-20 11:46:09 +0200

    Initial Commit

---
+++ NavigationStackExample/NavigationStackExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackExampleApp.swift
+//  NavigationStackExample
+//
+//  Created by Safwen DEBBICHI on 20/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
